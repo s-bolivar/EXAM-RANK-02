deae3d777c8f299864f273e1d9304ec55510fef8
s-bolivar <bolivarss1212@gmail.com>
2025-02-10 17:10:03 +0100

    Add files via upload

---
+++ borrar/EXAMS/EXAM02/sort_int_tab.c
@@ -0,0 +1,36 @@
+void sort_int_tab(int *tab, int size)
+{
+    int i = 0;
+    int j;
+    int temp;
+
+    while (i < size - 1)
+   {
+    j = 0;
+    while (j < size - i - 1)
+    {
+        if (tab[j] > tab[j + 1])
+        {
+            temp = tab[j];
+            tab[j] = tab[j + 1];
+            tab[j + 1] = temp;
+        }
+        j++;
+    }
+    i++;
+   } 
+}
+
+int main()
+{
+    int i = 0;
+    int tab[] = {1, 5, 2, 6, 2, 6, 3, 9, 8};
+    int size = sizeof(tab) / sizeof(tab[0]);
+
+    sort_int_tab(tab, size);
+    while (i < size)
+    {
+        printf("%d ", tab[i]);
+        i++;
+    }
+}
